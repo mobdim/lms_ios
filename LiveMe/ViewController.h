65192485a8faf7f9d49b7d3af2db9814b1e512ff
chetankaushal <admin@Parkin-Patel.local>
2015-03-22 23:48:45 -0500

    1. Record Video and Audio frame by frame

--- LiveMe/ViewController.h
+++ LiveMe/ViewController.h
@@ -5,11 +5,31 @@
 //  Created by Admin on 3/22/15.
 //  Copyright (c) 2015 Admin. All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+#import <Foundation/Foundation.h>
+#import <CoreMedia/CoreMedia.h>
+#import <AVFoundation/AVFoundation.h>
 
+#import <AssetsLibrary/AssetsLibrary.h>		//<<Can delete if not storing videos to the photo library.  Delete the assetslibrary framework too requires this)
+
+#define CAPTURE_FRAMES_PER_SECOND		20
+
+@interface ViewController : UIViewController <AVCaptureFileOutputRecordingDelegate>
+{
+    BOOL WeAreRecording;
+    
+    AVCaptureSession *CaptureSession;
+    AVCaptureMovieFileOutput *MovieFileOutput;
+    AVCaptureDeviceInput *VideoInputDevice;
+}
+
+@property (retain) AVCaptureVideoPreviewLayer *PreviewLayer;
+
+- (void) CameraSetOutputProperties;
+- (AVCaptureDevice *) CameraWithPosition:(AVCaptureDevicePosition) Position;
+- (IBAction)StartStopButtonPressed:(id)sender;
+- (IBAction)CameraToggleButtonPressed:(id)sender;
 
 @end
 
